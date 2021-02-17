e3eec518d7c308418b0573757e6356591b567ae7
Anastasia2005-coder <nastya.albu.05@mail.ru>
2021-02-17 19:12:40 +0300

    2-nd task done

--- list_friends_b.py
+++ list_friends_b.py
@@ -2,7 +2,10 @@ friends=[]
 while len(friends)<5:
     name=input("Enter name ")
     # проверить если переменная name не в списке
-    friends.append(name)
+    if name not in friends:
+        friends.append(name)
+    else:
+        print("Ошибка")
 
 # отсортировать по алфавиту 
 sorted_friends = sorted(friends)
