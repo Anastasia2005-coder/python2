67ce03b26aa0ab9affdbce86d212bb7de65038c1
Anastasia2005-coder <nastya.albu.05@mail.ru>
2021-02-20 12:40:21 +0300

    Измененная версия

--- list_friends_b.py
+++ list_friends_b.py
@@ -12,5 +12,5 @@ sorted_friends = sorted(friends)
 # вывести при помощи цыкла for in range 
 # print(sorted_friends)
 
-for name in range(1):
-    print(sorted_friends)
+for i in range(5):
+    print(sorted_friends[i])
